d313161a0e8af50efe6d20ac9d52a78a4eaf4e5b
Alp Hancioglu <alpi1997@hotmail.com>
2022-09-26 19:37:26 +1000

    Project Euler solved 2nd problem.

--- SolvesPE/SolvesPE.cpp
+++ SolvesPE/SolvesPE.cpp
@@ -1,5 +1,35 @@
 #include<iostream>
 
+int multiplesOf3Or5();
+int evenFibonacciNumbers();
+int main()
+{
+	int result;
+	result = evenFibonacciNumbers();
+	std::cout << result;
+	return 0;
+}
+
+int evenFibonacciNumbers()
+{
+	int a=0;
+	int b = 0;
+	int c = 1;
+	int sum = 0;
+	
+	while (a <= 4000000)
+	{
+		a = b + c;
+		if (a % 2 == 0)
+		{
+			sum += a;
+		}
+		b = c;
+		c = a;
+	}
+	return sum;
+}
+
 int multiplesOf3Or5()
 {
 	int sum = 0;
@@ -12,12 +42,3 @@ int multiplesOf3Or5()
 	}
 	return sum;
 }
-
-int main()
-{
-	int result;
-	result = multiplesOf3Or5();
-	std::cout << result;
-	return 0;
-}
-
