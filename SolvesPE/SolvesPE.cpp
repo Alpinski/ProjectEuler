39b1c677bd6d3d6a900d2c5b16276a79f80200d1
Alp Hancioglu <alpi1997@hotmail.com>
2022-09-27 19:18:44 +1000

    Project Euler 4th problem solved.

--- SolvesPE/SolvesPE.cpp
+++ SolvesPE/SolvesPE.cpp
@@ -1,17 +1,63 @@
 #include<iostream>
+#include<math.h>
+#include<limits>
+
 
 int multiplesOf3Or5();
 int evenFibonacciNumbers();
 void largestPrimeFactor(long long n);
-int largestPrimeFactor1(long long n);
+long long int largestPrimeFactor1(long long n);
+int largestPalindromeProduct(int input1);
+
+using namespace std;
 
 int main()
 {
-	int result = largestPrimeFactor1(600851475143);
+	int a = 0;
+	int b = 0;
+	int result = largestPalindromeProduct(a);
 	std::cout << result << std::endl;
 }
 
-int largestPrimeFactor1(long long n)
+int largestPalindromeProduct(int input1)
+{
+	//998001
+	cout << "Enter a number please: ";
+	cin >> input1;
+
+	int a = 0;
+	int b = 0;
+	long int num = 0;
+	long int max = 0;
+	if (!cin.fail())
+	{
+		for (int i = input1; i <= 9999999; i++)
+		{
+			b = 0;
+			num = i;
+			while (num > 0)
+			{
+				a = num % 10;
+				b = b * 10 + a;
+				num = num / 10;
+			}
+			if (b == i) {
+				//cout<<b<<endl;   //uncomment this line if you intend to display all palindromes
+				if (i > max)
+					max = i;
+			}
+			//cout << max << endl;
+		}
+		return max;
+	}
+	else
+	{
+		cout << "ENTER A NUMBER!" << endl;
+		return NULL;
+	}
+}
+
+long long int largestPrimeFactor1(long long n)
 {
 	long long a = 2;
 	long long b = 0;
